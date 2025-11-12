68c0009d8aba8f9a8bb44101622c18e902407109
StevenNZ <listevennz@gmail.com>
2025-11-12 20:47:09 +1300

    feat: bird moves up

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRigidbody.linearVelocity = Vector2.up * 10;
     }
 }
